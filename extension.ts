de042b22ecc2eca0175c654c89a3fec30588be87
Deniz Güney Yıldırım <31777283+denizguney@users.noreply.github.com>
2026-06-20 14:44:10 +0300

    sql.ts
    Added a function to check for SQL injection patterns in text documents.

--- extension.ts
+++ extension.ts
@@ -5,3 +5,25 @@
 -crypto.ts
 -cors.ts
 extension.ts
+
+//extension.ts içine eklenecek mantık
+export function checkSqlInjection(document: vscode.TextDocument, diagnosticCollection: vscode.DiagnosticCollection){
+  const text = document.getText();
+  const diagnostics: vscode.Diagnostic[] = [];
+
+//Basit bir örnek String birleştirme ile SQL yazımını yakala
+const sqlInjectionPattern = /db\.query\s*\(\s*["']SELECT.*?\+.*?\*.*?["']\S*\)/g;
+
+let match;
+  while((match = sqlInjectionPattern.exec(text)) !== null){
+    const startPos = document.positionAt(match.index);
+    const endPos = document.positionAt(match.index + match[0].length);
+
+  const diagnostic = new vscode.Diagnostic(
+    new vscode.Range(startPos, endPos), 
+    vscode.DiagnosticSeverity.Errror
+    );
+    diagnostics.push(diagnostic);
+  }
+  diagnosticCollection.set(document.uri , diagnostics);
+}
